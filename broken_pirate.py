10c098c33694d4e6032e0f673dda9377bf2422c5
suhayl <suhayl531@gmail.com>
2025-03-03 08:32:09 +0400

    SYNTAX ERRORS FIXED

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,5 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
+greeting = input("Hello, possible pirate! What's the password? ") # (Mohammed suhayl, M01033117) fixed the missing closing quote
+if greeting in ["Arrr!"]:#suhayl replaced ) by ]
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+else: # (Mohammed suhayl, m01033117) instead of elif should be else
+	print("Greetings, hater of pirates!") # (Mohammed suhayl, m01033117) print() is incorrectly idented and it was inside the if block
--- broken_travel.py
+++ broken_travel.py
@@ -6,11 +6,11 @@
 # if he is from the distant past (before 1900),
 # the present era (1900-2020) or from the far future (beyond 2020).
 
-year == int.input("Greetings! What is your year of origin? '))
+year = int(input("Greetings! What is your year of origin?")) # (Mohammed suhayl, m01033117) year == int.input should be year = int(input and fixed mismatch quote
 
-if year <= 1900
-    print ('Woah, that's the past!')
-elif year > 1900 && year < 2020:
+if year <= 1900: #(Mohammed suhayl,M01033117) fixed missing semicolon
+    print ("Woah, that's the past!")#(Mohammed suhayl,M01033117) fixed incoorect quote
+elif 1900 <= year < 2020: # (Mohammed suhayl,M01033117) incorrect and operator and fixed logic error
     print ("That's totally the present!")
-elif:
+else: # (Mohammed suhayl, M01033117) instead of elif it should be else
     print ("Far out, that's the future!!")
